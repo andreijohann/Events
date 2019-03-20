593c2417a824a8fc0b11a7472c268a1f575b747a
Andrei Johann <andrei_johann@yahoo.com>
2019-03-19 22:46:01 -0300

    DI with Unity

---
+++ Events.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Events.Core;
+
+namespace Events.Web.App_Start
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IUnitOfWork, UnitOfWork>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
